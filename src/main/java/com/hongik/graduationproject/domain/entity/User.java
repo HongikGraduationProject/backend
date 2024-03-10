ded0748b0f7938e83a450350916f11e37fba7899
yunhacandy <yunhacandy@g.hongik.ac.kr>
2024-03-10 23:59:54 +0900

    Refactor: RDB 이전을 위한 User 엔티티 재생성

--- src/main/java/com/hongik/graduationproject/domain/entity/User.java
+++ src/main/java/com/hongik/graduationproject/domain/entity/User.java
@@ -1,22 +1,30 @@
 package com.hongik.graduationproject.domain.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.CreatedDate;
 
 @Getter
-@Document(collection = "user")
+@Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Table(name = "user")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private String id;
+
     private String kakaoNickname;
     private Long kakaoId;
     private String email;
     private String nickname;
+
+    @CreatedDate
+    private LocalDateTime createAt;
 }
