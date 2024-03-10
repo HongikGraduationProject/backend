5aa36f6639fdbf4ecb01dbc1726f2ad58926a79d
yunhacandy <yunhacandy@g.hongik.ac.kr>
2024-03-11 00:13:19 +0900

    Refactor: RDB 이전을 위한 UserRepository 수정

--- src/main/java/com/hongik/graduationproject/repository/UserRepository.java
+++ src/main/java/com/hongik/graduationproject/repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.hongik.graduationproject.repository;
 
 import com.hongik.graduationproject.domain.entity.User;
 import java.util.Optional;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
 
     public User findByEmail(String email);
     public User findByKakaoId(Long kakaoId);
