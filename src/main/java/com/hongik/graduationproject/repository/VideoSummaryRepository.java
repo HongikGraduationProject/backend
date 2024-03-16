6a29c57f215a640d66c956eb83915356409fa613
qjvk2880 <qjvk2880@gmail.com>
2024-03-16 16:22:04 +0900

    Refactor: video summary를 jpa repository로 변경

--- src/main/java/com/hongik/graduationproject/repository/VideoSummaryRepository.java
+++ src/main/java/com/hongik/graduationproject/repository/VideoSummaryRepository.java
@@ -1,11 +1,11 @@
 package com.hongik.graduationproject.repository;
 
-import com.hongik.graduationproject.domain.entity.VideoSummary;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import com.hongik.graduationproject.domain.entity.VideoSummaryRDB;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface VideoSummaryRepository extends MongoRepository<VideoSummary, String> {
-    Optional<VideoSummary> findByUuid(String uuid);
-    boolean existsByUuid(String uuid);
+public interface VideoSummaryRepository extends JpaRepository<VideoSummaryRDB, Long> {
+    Optional<VideoSummaryRDB> findById(Long id);
+    boolean existsById(Long id);
 }
