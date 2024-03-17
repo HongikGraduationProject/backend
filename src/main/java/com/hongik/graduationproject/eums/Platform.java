9fc66e188635896f3175e6a22b8d46ea24c8b638
qjvk2880 <qjvk2880@gmail.com>
2024-03-17 16:20:17 +0900

    Refactor: 플랫폼 enum에서 필드 삭제

--- src/main/java/com/hongik/graduationproject/eums/Platform.java
+++ src/main/java/com/hongik/graduationproject/eums/Platform.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Platform {
-    YOUTUBE("YOUTUBE"),
-    INSTAGRAM("INSTAGRAM"),
-    TIKTOK("TIKTOK");
-    private final String name;
-
+    YOUTUBE,
+    INSTAGRAM,
+    TIKTOK
 }
