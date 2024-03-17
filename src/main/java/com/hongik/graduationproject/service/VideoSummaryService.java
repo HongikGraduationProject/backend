81a75d2a4302ebc494355ee63b60e37d7ec67527
qjvk2880 <qjvk2880@gmail.com>
2024-03-17 16:21:10 +0900

    Refactor: videoCode 생성 메소드 보강

--- src/main/java/com/hongik/graduationproject/service/VideoSummaryService.java
+++ src/main/java/com/hongik/graduationproject/service/VideoSummaryService.java
@@ -2,6 +2,7 @@ package com.hongik.graduationproject.service;
 
 import com.hongik.graduationproject.domain.dto.video.VideoSummaryInitiateRequest;
 import com.hongik.graduationproject.domain.dto.video.VideoSummaryInitiateResponse;
+import com.hongik.graduationproject.eums.Platform;
 import com.hongik.graduationproject.repository.VideoSummaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,8 +20,11 @@ public class VideoSummaryService {
 
     public VideoSummaryInitiateResponse sendUrlToQueue(VideoSummaryInitiateRequest videoSummaryInitiateRequest) {
         String videoCode = generateVideoCode(videoSummaryInitiateRequest.getUrl());
+
         videoSummaryInitiateRequest.setVideoCode(videoCode);
         messageService.sendVideoUrlToQueue(videoSummaryInitiateRequest);
+
+
         return new VideoSummaryInitiateResponse(videoCode);
     }
 
@@ -34,37 +38,49 @@ public class VideoSummaryService {
 //    }
 
     private String generateVideoCode(String url) {
-        String platformName = getVideoPlatform(url);
-        String videoId = getVideoId(url, platformName);
-        return platformName.concat(videoId);
+        Platform platform = getVideoPlatform(url);
+        String videoId = getVideoId(url, platform);
+        return platform.toString().concat("_").concat(videoId);
     }
 
-    private String getVideoId(String url, String platformName) {
-        String youtubeIdRegex = "(youtu.*be.*)\\/(watch\\?v=|embed\\/|v|shorts|)(.*?((?=[&#?])|$))";
+    private String getVideoId(String url, Platform platform) {
+        String idExtractRegex;
+        int idIndex;
 
-        if (platformName.equals(YOUTUBE.getName())) {
-            Pattern pattern = Pattern.compile(youtubeIdRegex);
-            Matcher matcher = pattern.matcher(url);
-            if (matcher.find()) {
-                return matcher.group(3);
-            }
-        } else if (platformName.equals(INSTAGRAM.getName())) {
+        switch (platform) {
+            case YOUTUBE:
+                idExtractRegex = "(youtu.*be.*)\\/(watch\\?v=|embed\\/|v|shorts|)(.*?((?=[&#?])|$))";
+                idIndex = 3;
+                break;
+            case INSTAGRAM:
+                idExtractRegex = "(?:https?:\\/\\/)?(?:www\\.)?instagram\\.com\\/?([a-zA-Z0-9\\.\\_\\-]+)?\\/([p]+)?([reel]+)?([tv]+)?([stories]+)?\\/([a-zA-Z0-9\\-\\_\\.]+)\\/?([0-9]+)?";
+                idIndex = 6;
+                break;
+            default:
+                throw new RuntimeException();
+        }
+
+        Pattern pattern = Pattern.compile(idExtractRegex);
+        Matcher matcher = pattern.matcher(url);
+        if (matcher.find()) {
+            return matcher.group(idIndex);
+        } else {
+            // TODO : 예외처리 요망
+            throw new RuntimeException();
 
         }
-        // TODO : 예외처리 요망
-        return "error";
     }
 
-    private String getVideoPlatform(String url) {
+    private Platform getVideoPlatform(String url) {
         String youtubeValidationRegex = "^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube(-nocookie)?\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|live\\/|v\\/)?)([\\w\\-]+)(\\S+)?$";
-        String instagramValidationRegex = "(?:(?:http|https):\\/\\/)?(?:www.)?(?:instagram.com|instagr.am|instagr.com)\\/(\\w+)";
+        String instagramValidationRegex = "https?:\\/\\/(?:www.)?instagram.com\\/reels?\\/([^\\/?#&]+).*";
         if (url.matches(youtubeValidationRegex)) {
-            return YOUTUBE.getName();
+            return YOUTUBE;
         } else if (url.matches(instagramValidationRegex)) {
-            return INSTAGRAM.getName();
+            return INSTAGRAM;
         } else {
             // TODO: 예외 처리 요망
-            return "error";
+            throw new RuntimeException();
         }
     }
 }
