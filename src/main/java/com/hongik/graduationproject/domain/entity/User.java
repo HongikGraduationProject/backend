1b311f72261271b83f1c70e252a4dda4807ab40d
yunhacandy <yunhacandy@g.hongik.ac.kr>
2024-03-17 19:42:03 +0900

    Refactor: id 자료형 수정

--- src/main/java/com/hongik/graduationproject/domain/entity/User.java
+++ src/main/java/com/hongik/graduationproject/domain/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private String id;
+    private Long id;
 
     private String kakaoNickname;
     private Long kakaoId;
