899bdbf74efc2f5302d2a444bbb71eef2a77aec9
qjvk2880 <qjvk2880@gmail.com>
2024-03-22 01:17:56 +0900

    Chore: MongoDB 의존성 제거

--- src/main/java/com/hongik/graduationproject/GraduationProjectApplication.java
+++ src/main/java/com/hongik/graduationproject/GraduationProjectApplication.java
@@ -4,12 +4,10 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
-@EnableMongoRepositories
 @EnableJpaAuditing
 public class GraduationProjectApplication {
     @PostConstruct
--- src/main/java/com/hongik/graduationproject/domain/entity/cache/VideoSummaryStatusCache.java
+++ src/main/java/com/hongik/graduationproject/domain/entity/cache/VideoSummaryStatusCache.java
@@ -1,13 +1,10 @@
 package com.hongik.graduationproject.domain.entity.cache;
 
-import jakarta.persistence.Index;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @AllArgsConstructor
