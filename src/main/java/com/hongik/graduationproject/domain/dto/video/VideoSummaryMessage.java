38eeacb434e865a21d4b69badf78013ea9eb0fc0
qjvk2880 <qjvk2880@gmail.com>
2024-03-27 22:24:41 +0900

    Feat: VideoSummaryMessage dto 추가

---
+++ src/main/java/com/hongik/graduationproject/domain/dto/video/VideoSummaryMessage.java
@@ -0,0 +1,23 @@
+package com.hongik.graduationproject.domain.dto.video;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoSummaryMessage {
+    @JsonProperty("video_code")
+    String videoCode;
+    String title;
+    String description;
+    List<String> keywords;
+    String url;
+    String summary;
+    String address;
+}
