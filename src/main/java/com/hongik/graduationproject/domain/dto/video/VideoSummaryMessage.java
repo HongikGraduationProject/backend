f96e725fe87155dc5fc0d6c6fb67bb763d0ee630
qjvk2880 <qjvk2880@gmail.com>
2024-03-27 23:21:34 +0900

    Feat: 메세지 큐로부터 카테고리 이름을 받게끔 수정

--- src/main/java/com/hongik/graduationproject/domain/dto/video/VideoSummaryMessage.java
+++ src/main/java/com/hongik/graduationproject/domain/dto/video/VideoSummaryMessage.java
@@ -20,4 +20,6 @@ public class VideoSummaryMessage {
     String url;
     String summary;
     String address;
+    @JsonProperty("video_code")
+    String categoryName;
 }
---
+++ src/main/java/com/hongik/graduationproject/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.hongik.graduationproject.repository;
+
+import com.hongik.graduationproject.domain.entity.Category;
+import com.hongik.graduationproject.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("select c from Category c where c.user.id = :userId")
+    Optional<Category> findByUserId(Long userId);
+}
