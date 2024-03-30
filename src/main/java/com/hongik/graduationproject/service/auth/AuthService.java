c6b69ff7b4301a8e5c0a6fe47a803153ba248564
yunhacandypark <yunhacandy@g.hongik.ac.kr>
2024-03-30 17:10:27 +0900

    Feat: 카카오 소셜 로그인 Reissue 기능 구현

--- src/main/java/com/hongik/graduationproject/service/auth/AuthService.java
+++ src/main/java/com/hongik/graduationproject/service/auth/AuthService.java
@@ -1,8 +1,11 @@
 package com.hongik.graduationproject.service.auth;
 
+import com.hongik.graduationproject.domain.dto.KaKaoRequestDto;
 import com.hongik.graduationproject.domain.dto.Response;
 import com.hongik.graduationproject.domain.dto.AuthRequestDto;
 
 public interface AuthService {
+
     Response<?> loginUser(AuthRequestDto authRequestDto);
+    Response<?> reissueToken(KaKaoRequestDto kaKaoRequestDto);
 }
