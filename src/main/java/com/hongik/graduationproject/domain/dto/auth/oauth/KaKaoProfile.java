ce423bbaf4bc740299de2115fa2a3618c8a04c4c
yunhacandypark <yunhacandy@g.hongik.ac.kr>
2024-04-05 00:45:54 +0900

    Refactor: 어노테이션 추가

--- src/main/java/com/hongik/graduationproject/domain/dto/auth/oauth/KaKaoProfile.java
+++ src/main/java/com/hongik/graduationproject/domain/dto/auth/oauth/KaKaoProfile.java
@@ -1,8 +1,12 @@
 package com.hongik.graduationproject.domain.dto.auth.oauth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class KaKaoProfile {
 
     public Long id;
