e9b06ba356057c7036c51b57a268bb8b617ca1ab
qjvk2880 <qjvk2880@gmail.com>
2024-04-04 16:55:58 +0900

    Docs: swagger 수정

--- src/main/java/com/hongik/graduationproject/domain/dto/video/VideoSummaryInitiateRequest.java
+++ src/main/java/com/hongik/graduationproject/domain/dto/video/VideoSummaryInitiateRequest.java
@@ -14,7 +14,7 @@ public class VideoSummaryInitiateRequest {
     String url;
     @Schema(description = "카테고리 아이디", nullable = true, example = "1")
     Long categoryId;
-    @Schema(description = "(임시) 사용자가 카테고리를 지정했는지 여부", example = "true")
+    @Schema(description = "사용자가 카테고리를 지정했는지 여부", example = "true")
     Boolean isCategoryIncluded;
-    Long userId;
+    Long userId = 1L;
 }
