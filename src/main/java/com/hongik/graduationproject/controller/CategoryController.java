1a7738495cd03edc5e3490e4c078b9ef1526347c
qjvk2880 <qjvk2880@gmail.com>
2024-04-11 12:23:50 +0900

    Feat: getSubCategoryList()에 swagger 적용

--- src/main/java/com/hongik/graduationproject/controller/CategoryController.java
+++ src/main/java/com/hongik/graduationproject/controller/CategoryController.java
@@ -1,11 +1,14 @@
 package com.hongik.graduationproject.controller;
 
 import com.hongik.graduationproject.domain.dto.Response;
-import com.hongik.graduationproject.domain.dto.category.SubCategoryListRequest;
 import com.hongik.graduationproject.domain.dto.category.SubCategoryListResponse;
+import com.hongik.graduationproject.domain.dto.video.VideoSummaryListResponse;
 import com.hongik.graduationproject.eum.MainCategory;
 import com.hongik.graduationproject.service.CategoryService;
-import lombok.Getter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryController {
     private final CategoryService categoryService;
 
+    @Operation(summary = "서브 카테고리 목록 조회", description = "사용자의 특정 메인 카테고리에 해당하는 서브 카테고리 목록 조회를 위한 메소드")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = SubCategoryListResponse.class)))
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/categories")
     public Response<SubCategoryListResponse> getSubCategoryList(@RequestParam MainCategory mainCategory) {
