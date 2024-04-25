75fdb691ebed00b2c299fa6c00b3bcfc79e4e99e
qjvk2880 <qjvk2880@gmail.com>
2024-04-25 18:33:21 +0900

    Fix: 충돌 해결를 위한 코드 수정

--- src/main/java/com/hongik/graduationproject/repository/UserRepository.java
+++ src/main/java/com/hongik/graduationproject/repository/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public User findByEmail(String email);
     public User findByKakaoId(Long kakaoId);
 
     Optional<User> findById(Long id);
