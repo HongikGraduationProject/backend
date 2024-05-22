69f5d19b9b2f1da262df3ec70383139584468cfc
qjvk2880 <qjvk2880@gmail.com>
2024-05-22 18:17:19 +0900

    Refactor: 불필요한 repository 메소드 제거

--- src/main/java/com/hongik/graduationproject/exception/ErrorCode.java
+++ src/main/java/com/hongik/graduationproject/exception/ErrorCode.java
@@ -11,8 +11,8 @@ public enum ErrorCode {
     ALREADY_REQUESTED_SUMMARIZING(HttpStatus.BAD_REQUEST, "사용자가 이미 해당 영상을 요약 요청했습니다."),
     FAILED_TO_EXTRACT_EXTRACT_ID(HttpStatus.BAD_REQUEST, "영상의 고유 ID를 추출하는데 실패했습니다."),
     VIDEO_SUMMARY_NOT_FOUND(HttpStatus.NOT_FOUND,"영상 요약 정보를 찾을 수 없습니다"),
-    MAIN_CATEGORY_NOT_EXISTS(HttpStatus.BAD_REQUEST,"해당 메인 카테고리가 존재하지 않습니다.");
-
+    MAIN_CATEGORY_NOT_EXISTS(HttpStatus.BAD_REQUEST,"해당 메인 카테고리가 존재하지 않습니다."),
+    SUMMARIZING_STATUS_NOT_EXIST(HttpStatus.NOT_FOUND, "요약 정보를 찾을 수 없습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
