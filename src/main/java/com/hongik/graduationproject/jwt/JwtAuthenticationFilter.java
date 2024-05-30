df9aac813001b991174c11db50f1ede730deb84c
qjvk2880 <qjvk2880@gmail.com>
2024-05-31 00:38:17 +0900

    Refactor: swagger에 대해 필터 예외 처리

--- src/main/java/com/hongik/graduationproject/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hongik/graduationproject/jwt/JwtAuthenticationFilter.java
@@ -38,7 +38,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getRequestURI();
-        return path.startsWith("/api/auth");
+        return path.startsWith("/api/auth") || path.startsWith("/swagger-ui");
     }
 
     private String parseBearerToken(HttpServletRequest request) {
