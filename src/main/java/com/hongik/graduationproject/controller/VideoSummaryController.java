7d7130feb47717fa7a92c002ea53bfcdec4c05f3
yunhacandy <yunhacandy@gmail.com>
2024-07-21 19:29:44 +0900

    refactor:`getAllSummariesByCategoryId` 메서드 수정
    - `getAllSummariesByCategoryId` 메서드에서 `categoryId` 파라미터를 `@RequestParam`에서 `@PathVariable`로 변경
    - `userId` 파라미터를 `@AuthenticationPrincipal`로 추가하여 인증된 사용자 정보를 받아오도록 수정
    - api path가 다른 로직과 충돌나지 않게 수정
    - api path에서 `categoryId`를 올바르게 추출하고, 인증된 사용자 정보를 활용하도록 수정

--- src/main/java/com/hongik/graduationproject/controller/VideoSummaryController.java
+++ src/main/java/com/hongik/graduationproject/controller/VideoSummaryController.java
@@ -53,19 +53,21 @@ public class VideoSummaryController {
     public Response<VideoSummaryDto> getSummaryByVideoSummaryId(@PathVariable(name = "videoSummaryId")
                                                                 @Parameter(name = "videoSummaryId", description = "영상 요약 상태에서 응답받은 videoSummaryId", example = "3")
                                                                 Long videoSummaryId,
-                                                                @AuthenticationPrincipal Long userId
-                                                                ) {
+                                                                @AuthenticationPrincipal Long userId) {
         log.info("summary requested videoSummaryId = {}", videoSummaryId);
         return Response.createSuccess(videoSummaryService.getSummaryByVideoSummaryId(videoSummaryId, userId));
     }
 
     @Operation(summary = "영상 요약 목록 조회", description = "categoryId로 영상 요약 목록 조회를 위한 메소드")
     @ApiResponse(content = @Content(schema = @Schema(implementation = VideoSummaryListResponse.class)))
-    @GetMapping("/summaries")
+    @GetMapping("/summaries/list/{categoryId}")
     @ResponseStatus(HttpStatus.OK)
-    public Response<VideoSummaryListResponse> getAllSummariesByCategoryId(@Parameter(required = true) @RequestParam Long categoryId) {
+    public Response<VideoSummaryListResponse> getAllSummariesByCategoryId(@PathVariable(name = "categoryId")
+                                                                          @Parameter(name = "categoryId", description = "영상 요약 목록을 조회하려는 categoryId", example = "1")
+                                                                          Long categoryId,
+                                                                          @AuthenticationPrincipal Long userId) {
         log.info("get all summaries for categoryId = {}", categoryId);
-        return Response.createSuccess(videoSummaryService.getAllSummariesByCategoryId(categoryId));
+        return Response.createSuccess(videoSummaryService.getAllSummariesByCategoryId(categoryId, userId));
     }
 
     @Operation(summary = "검색 결과 조회", description = "제목 또는 내용에서 검색어를 포함하는 숏폼 조회를 위한 API")
